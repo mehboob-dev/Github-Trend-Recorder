b1e9bee15f05baa858be87c84e97b0daa4f41155
Meghani M. Mehboob <suitable9004@gmail.com>
2021-06-24 23:15:49 +0530

    fixed route

--- app.py
+++ app.py
@@ -5,24 +5,30 @@ Created on Thu Jun 17 01:58:14 2021
 @author: Admin
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 import datafilteration as fdb
 
 app = Flask(__name__)
 
+clangs = fdb.getallclang()
+datas = fdb.getalldata().head(10).to_records()
+
 @app.route("/")
+def maintemp():
+    global clangs, datas
+    return render_template("index.html", clangs=clangs[0:12], datas=datas)
+
+@app.route("/news")
 def main():
+    global clangs, datas
     clangs = fdb.getallclang()
-    datas = fdb.getalldata().head(10).to_records()
-    return render_template("index.html", clangs=clangs[0:10], datas=datas)
-
-
-@app.route("/clang/<lang>")
-def clang(lang):
-    langs = [lang]
-    datas = fdb.getallbycodelang(lang, "TrendStar").head(10).to_records()
-    return render_template("index.html", clangs=langs, datas=datas)
+    if len(request.args)>0:
+        lang = request.args["reqlang"]
+        datas = fdb.getallbycodelang(lang, "TrendStar").head(10).to_records()
+    else:
+        datas = fdb.getalldata().head(10).to_records()
+    return redirect(url_for("maintemp"))
 
 
 if __name__ == "__main__":
